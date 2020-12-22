0e6a8969419337e81131d8f17bd3db9b0e7090c7
Zvat Scorobogatov <zvat.333333@gmail.com>
2020-12-22 21:35:27 +0200

    add some new

--- src/class-works/cw2/life-cycle-example/components/CommentList.js
+++ src/class-works/cw2/life-cycle-example/components/CommentList.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import CommentItem from './CommentItem';
+import LoadingOverlay from './LoadingOverlay'
 
 export default class CommentList extends React.Component {
     constructor(props){
@@ -18,7 +19,7 @@ export default class CommentList extends React.Component {
     }
 
     fetchPostComments(PostId){
-        
+        this.setState({ loading: true });
         fetch(`https://jsonplaceholder.typicode.com/posts/${PostId}/comments`)
             .then(response => response.json())
             .then(comments => {
@@ -27,13 +28,20 @@ export default class CommentList extends React.Component {
                         comments
                 })
             })
+            .catch(e => {
+                this.setState({
+                  loading: false
+                });
+                alert(e.message)
+              })
     }
    
     render() {
-        const {comments} = this.state;
+        const {comments, loading} = this.state;
         
         return (
             <div className={"comment-item"}>
+                 <LoadingOverlay active={loading} />
                { comments.map( Comment => <CommentItem comment = {Comment} key={comments.id}/>)}
             </div>
         )
